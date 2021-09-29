9e95fec3fb3b64d546a636f66e32ce5c1da3ce1c
stephanietsou <82925550+stephanietsou@users.noreply.github.com>
2021-09-29 19:13:13 -0400

    adding javascript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // $(".logo").hover(function(){
+    //     $(".logo").addClass(".rotate");
+    // });
+
+    $(".intro").hover(function(){
+        $("h1").addClass(".green");
+    })
+
+
+
+}); //end of document.ready function
